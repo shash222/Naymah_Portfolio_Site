3c095f89719350dbc0d87a1853b9171efe1138e4
root <root@hashmi.site>
2020-06-09 23:18:27 +0000

    Updated content

--- navigationbar.js
+++ navigationbar.js
@@ -1,118 +1,128 @@
 /*Shrink Header*/
-var shrunk=0;
-window.onscroll=function(){
-	var headerAnimationShrink=[
-		{height:'30%'},
-		{height:'15%'},
+var shrunk = 0;
+window.onscroll = function () {
+	var headerAnimationShrink = [
+		{ height: '30%' },
+		{ height: '15%' },
 	];
-	var headerAnimationGrow=[
-		{height:'15%'},
-		{height:'30%'},
+	var headerAnimationGrow = [
+		{ height: '15%' },
+		{ height: '30%' },
 	];
-	var imageAnimationShrink=[
-		{right:'75%'},
-		{right:'65%'},
+	var imageAnimationShrink = [
+		{ right: '75%' },
+		{ right: '65%' },
 	]
-	var imageAnimationGrow=[
-		{right:'65%'},
-		{right:'75%'},
+	var imageAnimationGrow = [
+		{ right: '65%' },
+		{ right: '75%' },
 	]
-	var nameAnimationShrink=[
-		{right:'20%',
-		 top:'15px'},
-		{right:'23%',
-		top:'10px'}
+	var nameAnimationShrink = [
+		{
+			right: '20%',
+			top: '15px'
+		},
+		{
+			right: '23%',
+			top: '10px'
+		}
 	]
-	var nameAnimationGrow=[
-		{right:'23%',
-		 top:'10px'},
-		{right:'20%',
-		 top:'15px'}
+	var nameAnimationGrow = [
+		{
+			right: '23%',
+			top: '10px'
+		},
+		{
+			right: '20%',
+			top: '15px'
+		}
 	]
-	var timing={
-		duration:300,
-		iterations:1,
-		fill:"forwards"
+	var timing = {
+		duration: 300,
+		iterations: 1,
+		fill: "forwards"
 	};
-	if (window.scrollY>window.innerHeight*.1 && shrunk==0){
-		document.getElementsByTagName("header")[0].animate(headerAnimationShrink,timing);
-		document.getElementById("image").animate(imageAnimationShrink,timing);
-		document.getElementById("name").animate(nameAnimationShrink,timing);
-		document.getElementById("objective").innerHTML="";
-		shrunk=1;
+	if (window.scrollY > window.innerHeight * .1 && shrunk == 0) {
+		document.getElementsByTagName("header")[0].animate(headerAnimationShrink, timing);
+		document.getElementById("image").animate(imageAnimationShrink, timing);
+		document.getElementById("name").animate(nameAnimationShrink, timing);
+		document.getElementById("objective").innerHTML = "";
+		shrunk = 1;
 	}
-	else if (window.scrollY<window.innerHeight*.1 && shrunk==1){
-		document.getElementsByTagName("header")[0].animate(headerAnimationGrow,timing);
-		document.getElementById("image").animate(imageAnimationGrow,timing);
-		document.getElementById("name").animate(nameAnimationGrow,timing);
-		setTimeout(innerHTML,250);
-   		shrunk=0;
+	else if (window.scrollY < window.innerHeight * .1 && shrunk == 1) {
+		document.getElementsByTagName("header")[0].animate(headerAnimationGrow, timing);
+		document.getElementById("image").animate(imageAnimationGrow, timing);
+		document.getElementById("name").animate(nameAnimationGrow, timing);
+		setTimeout(innerHTML, 250);
+		shrunk = 0;
 	}
-	function innerHTML(){
-		document.getElementById("objective").innerHTML="I am an Architecture graduate from NJIT with a passion for interior design, architecture, and urban design and also excel in multiple architecture softwares."
-	}
+}
+
+function innerHTML() {
+	document.getElementById("objective").innerHTML = "I am an Architecture graduate from NJIT with a passion for interior design, architecture, and urban design. Skilled with design-forward thinking, I want to utilize it to help those in need."
 }
 
 /*Automatic SlideShow*/
-var imagenumber=0;
-window.onload=function(){
+var imagenumber = 0;
+window.onload = function () {
+	this.innerHTML()
 	// Slideshow 1
 	slideshow();
-	function slideshow(){
-		var pictures=document.getElementsByClassName("slides");
-		var descriptions=document.getElementsByClassName("slideshowDescriptions");
-		var titles=document.getElementsByClassName("slideshowTitles");
-		for (i=0;i<pictures.length;i++){
-			pictures[i].style.display="none";
-			if (descriptions.length != 0) descriptions[i].style.display="none";
-			if (titles.length != 0) titles[i].style.display="none";
+	function slideshow() {
+		var pictures = document.getElementsByClassName("slides");
+		var descriptions = document.getElementsByClassName("slideshowDescriptions");
+		var titles = document.getElementsByClassName("slideshowTitles");
+		for (i = 0; i < pictures.length; i++) {
+			pictures[i].style.display = "none";
+			if (descriptions.length != 0) descriptions[i].style.display = "none";
+			if (titles.length != 0) titles[i].style.display = "none";
 
 
 		}
 		imagenumber++;
-		if (imagenumber>pictures.length){
-			imagenumber=1;
+		if (imagenumber > pictures.length) {
+			imagenumber = 1;
 		}
-		pictures[imagenumber-1].style.display="block";
-		if (descriptions.length != 0) descriptions[imagenumber-1].style.display="block";
-		if (titles.length != 0) titles[imagenumber-1].style.display="block";
-		setTimeout(slideshow,5000);
+		pictures[imagenumber - 1].style.display = "block";
+		if (descriptions.length != 0) descriptions[imagenumber - 1].style.display = "block";
+		if (titles.length != 0) titles[imagenumber - 1].style.display = "block";
+		setTimeout(slideshow, 5000);
 	}
-        
+
 }
 
 
-/*Portfolio Page*/	
-function portfolioPage(){
+/*Portfolio Page*/
+function portfolioPage() {
 	console.log("Portfolio is working");
-	var titles=document.getElementsByClassName("title");
-	for (let i=0;i<titles.length;i++){
-		var height=titles[i].clientHeight;
-		titles[i].style.transform="translateX("+(height)+"px) rotate(90deg)";
-		titles[i].style.transformOrigin="top left";
+	var titles = document.getElementsByClassName("title");
+	for (let i = 0; i < titles.length; i++) {
+		var height = titles[i].clientHeight;
+		titles[i].style.transform = "translateX(" + (height) + "px) rotate(90deg)";
+		titles[i].style.transformOrigin = "top left";
 	}
 
 }
-	
 
 
-function manualslideshow(buttonid,slideshowclass){
-	var pictures=document.getElementsByClassName(slideshowclass);
-	for (i=0;i<pictures.length;i++){
-		pictures[i].style.display="none";
+
+function manualslideshow(buttonid, slideshowclass) {
+	var pictures = document.getElementsByClassName(slideshowclass);
+	for (i = 0; i < pictures.length; i++) {
+		pictures[i].style.display = "none";
 	}
-	if (buttonid=="rightButton"){
+	if (buttonid == "rightButton") {
 		imagenumber++;
 	}
-	else if (buttonid=="leftButton"){
+	else if (buttonid == "leftButton") {
 		imagenumber--;
 	}
-	if (imagenumber>pictures.length){
-		imagenumber=1;
+	if (imagenumber > pictures.length) {
+		imagenumber = 1;
 	}
-	if (imagenumber<1){
-		imagenumber=pictures.length;
+	if (imagenumber < 1) {
+		imagenumber = pictures.length;
 	}
-	pictures[imagenumber-1].style.display="block";
+	pictures[imagenumber - 1].style.display = "block";
 }
 
